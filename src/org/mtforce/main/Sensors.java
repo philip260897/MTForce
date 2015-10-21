fe887337d32c1af56472d78b7f8513436971d2a3
Philip Lindner <philip260897@gmail.com>
2015-10-21 15:50:18 +0200

    Sensors class added

---
+++ src/org/mtforce/main/Sensors.java
@@ -0,0 +1,14 @@
+package org.mtforce.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sensors 
+{
+	private List<Sensor> sensorList = new ArrayList<Sensor>();
+	
+	public Sensors()
+	{
+		
+	}
+}
