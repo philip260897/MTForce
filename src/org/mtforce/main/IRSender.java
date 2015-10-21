3b8b98a20b2edf47044a3d0df892839558e90afa
Ben Brix <de.mon@live.at>
2015-10-21 15:48:42 +0200

    Implements Sensor Interface

--- src/org/mtforce/main/IRSender.java
+++ src/org/mtforce/main/IRSender.java
@@ -1,10 +1,28 @@
 package org.mtforce.main;
 
-public class IRSender {
+public class IRSender implements Sensor{
 	private int start;
 	private double data;
 	public double setOn() {
 		data = GPIOManager.write();
 		return data;
+	}
+	
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
 	}
 }
