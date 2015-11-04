464ee195f9b9d5bd269ba18d59bb6274dcb6e822
Philip Lindner <philip260897@gmail.com>
2015-11-04 14:33:27 +0100

    Comment IOExpander

--- src/org/mtforce/sensors/IOExpander.java
+++ src/org/mtforce/sensors/IOExpander.java
@@ -9,14 +9,19 @@ import com.pi4j.io.i2c.I2CFactory;
 
 public class IOExpander extends Disableable implements Sensor
 {
+	//Address and common register definition
     private int ADDRESS =	0x20;
     private int GPIOA	=	0x14;
    
+    //track current LED State
     private boolean ledState = false;
 	
 	@Override
-	public void init() {
-
+	public void init() 
+	{
+		//Gets called on Startup once. Check if Sensor is available and setEnabled(true);
+		
+		//Returns false if component not reachable
 		if(I2CManager.write(ADDRESS, GPIOA, 0x00)) 
 		{
 			setEnabled(true);
@@ -25,27 +30,31 @@ public class IOExpander extends Disableable implements Sensor
 		else 
 		{
 			System.out.println(this.getClass().getSimpleName() + ": init error! Device not functional");
-		}
+		} 
 	}
 
 	@Override
 	public void update() {
+		//Call this method to update specific data. Used to read from component and update tracking variables
 		if(!isEnabled())
 			return;
 
+		
+		
 	}
 
 	public void setLedOn(boolean led)
 	{
+		//Check if Sensor is enabled or not
 		if(!isEnabled())
 			return;
 		
-		byte value = 0x01;
-		if(!led)
-			value = 0x00;
-		ledState = led;
+		//register value if led is true(0x01) or false(0x00)
+		byte value = (byte) (led ? 0x01 : 0x00);
 		
+		//write value to component
 		I2CManager.write(ADDRESS, GPIOA, value);
+		ledState = led;
 	}
 	
 	public boolean getLedOn() {
@@ -54,9 +63,11 @@ public class IOExpander extends Disableable implements Sensor
 	
 	@Override
 	public void dispose() {
+		//Gets called on shutdown
 		if(!isEnabled())
 			return;
 		
+		//Turn off the LED on shutdown
 		I2CManager.write(ADDRESS, GPIOA, 0x00);
 	}
 
