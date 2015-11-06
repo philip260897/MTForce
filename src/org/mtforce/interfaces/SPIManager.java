eddc54af8c22be72457daa4472743208262949d1
Philip Lindner <philip260897@gmail.com>
2015-11-06 14:28:13 +0100

    T

--- src/org/mtforce/interfaces/SPIManager.java
+++ src/org/mtforce/interfaces/SPIManager.java
@@ -24,6 +24,7 @@ public class SPIManager
 		try 
 		{
 			return spi.write(packets);
+			
 		}
 		catch (IOException e) 
 		{
--- src/org/mtforce/sensors/Ser7Seg.java
+++ src/org/mtforce/sensors/Ser7Seg.java
@@ -1,53 +1,62 @@
 package org.mtforce.sensors;
 
+import java.util.Calendar;
+
 import org.mtforce.interfaces.SPIManager;
 
-public class Ser7Seg implements Sensor
+public class Ser7Seg extends Disableable implements Sensor
 {
-	int maxInUse = 1; 
-	// define max7219 registers
-	byte max7219_reg_noop        = 0x00;
-	byte max7219_reg_digit0      = 0x01;
-	byte max7219_reg_digit1      = 0x02;
-	byte max7219_reg_digit2      = 0x03;
-	byte max7219_reg_digit3      = 0x04;
-	byte max7219_reg_digit4      = 0x05;
-	byte max7219_reg_digit5      = 0x06;
-	byte max7219_reg_digit6      = 0x07;
-	byte max7219_reg_digit7      = 0x08;
-	byte max7219_reg_decodeMode  = 0x09;
-	byte max7219_reg_intensity   = 0x0a;
-	byte max7219_reg_scanLimit   = 0x0b;
-	byte max7219_reg_shutdown    = 0x0c;
-	byte max7219_reg_displayTest = 0x0f;
+	//Common register declaration 
+	private byte max7219_reg_noop        = 0x00;
+	private byte max7219_reg_digit0      = 0x01;
+	private byte max7219_reg_digit1      = 0x02;
+	private byte max7219_reg_digit2      = 0x03;
+	private byte max7219_reg_digit3      = 0x04;
+	private byte max7219_reg_digit4      = 0x05;
+	private byte max7219_reg_digit5      = 0x06;
+	private byte max7219_reg_digit6      = 0x07;
+	private byte max7219_reg_digit7      = 0x08;
+	private byte max7219_reg_decodeMode  = 0x09;
+	private byte max7219_reg_intensity   = 0x0a;
+	private byte max7219_reg_scanLimit   = 0x0b;
+	private byte max7219_reg_shutdown    = 0x0c;
+	private byte max7219_reg_displayTest = 0x0f;
 
-	byte digits[] = {0,0,0,0,0 ,0,0,0};
+	private byte digits[] = {0,0,0,0,0 ,0,0,0};
 	
 	private int number = 0;
+	private int intensity = 3;
+	
+	private Calendar cal = Calendar.getInstance();
 
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
 		//initiation of the max 7219
 		SPIManager.write(max7219_reg_scanLimit, 	(byte)0x07);      
-		SPIManager.write(max7219_reg_decodeMode, 	(byte)0xff);  // using an digits
-		SPIManager.write(max7219_reg_shutdown, 		(byte)0x01);    // not in shutdown mode
-		SPIManager.write(max7219_reg_displayTest, 	(byte)0x00); // no display test
+		SPIManager.write(max7219_reg_decodeMode, 	(byte)0xdb); 	// using an digits
+		SPIManager.write(max7219_reg_shutdown, 		(byte)0x01);	// not in shutdown mode
+		SPIManager.write(max7219_reg_displayTest, 	(byte)0x00);	// no display test
 		
+		//Set all digits to 0
 		for (int e=1; e<=8; e++) 
 		{ 
 			SPIManager.write((byte)e,(byte)0);
 		}
-		SPIManager.write(max7219_reg_intensity, (byte)((byte)0x01 & (byte)0x0f));    // the first 0x0f is the value you can set
-	
-
+		//SPIManager.write(max7219_reg_intensity, (byte)((byte)0x01 & (byte)0x0f));    // the first 0x0f is the value you can set
+		SPIManager.write(max7219_reg_intensity, (byte)intensity);
+		setEnabled(true);
 	}
 
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
 		number++;
-		printNumber(number, 0);
+		
+		clearDigits();
+		cal = Calendar.getInstance();
+		printNumber(cal.getTime().getHours(), 6);
+		printNumber(cal.getTime().getMinutes(), 3);
+		printNumber(cal.getTime().getSeconds(), 0);
 	}
 
 
@@ -60,6 +69,27 @@ public class Ser7Seg implements Sensor
 
 	public int getNumber() {
 		return number;
+	}
+	
+	public void setNumber(int number) {
+		if(!isEnabled())
+			return;
+		
+		this.number = number;
+		
+		printNumber(number, 0);
+	}
+	
+	public int getIntensity() {
+		return intensity;
+	}
+	
+	public void setIntensity(int intensity) {
+		if(!isEnabled())
+			return;
+		
+		this.intensity = intensity;
+		
 	}
 	
 	private void printNumber(int number, int offset)
@@ -78,6 +108,8 @@ public class Ser7Seg implements Sensor
 	      i++;
 	   }
 
+	   //updateUnusedDigits();
+	   
 	   SPIManager.write(max7219_reg_digit0, digits[0]);
 	   SPIManager.write(max7219_reg_digit1, digits[1]);
 	   SPIManager.write(max7219_reg_digit2, digits[2]);
@@ -86,5 +118,28 @@ public class Ser7Seg implements Sensor
 	   SPIManager.write(max7219_reg_digit5, digits[5]);
 	   SPIManager.write(max7219_reg_digit6, digits[6]);
 	   SPIManager.write(max7219_reg_digit7, digits[7]);
+	   
+	   
+	}
+	
+	private void updateUnusedDigits()
+	{
+		int c = 7;
+		for(int i = 7; i >= 0; i--)
+		{
+			if(digits[i] == 0) 
+				c--;
+			else
+				i = -1;
+			
+		}
+		SPIManager.write(this.max7219_reg_scanLimit, (byte)c);
+		SPIManager.write(max7219_reg_intensity, (byte)intensity);
+	}
+	
+	private void clearDigits() 
+	{
+		for(int i = 0; i < 8; i++)
+			digits[i] = 0;
 	}
 }
