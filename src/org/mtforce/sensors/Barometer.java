3c6c2ad0f16d3651849378a0251d0f8d78a20775
Ben Brix <de.mon@live.at>
2015-11-09 15:48:14 +0100

    implemented constants

--- src/org/mtforce/sensors/Barometer.java
+++ src/org/mtforce/sensors/Barometer.java
@@ -2,6 +2,13 @@ package org.mtforce.sensors;
 
 public class Barometer extends Sensor {
 
+	public static final int ADDRESS = 0x76; //LAST BIT = !CSB (CSB = HARDWARE PIN)
+	public static final int RESET 	= 0x1E;
+	
+	public static final int PROM_READ = 0xA6;
+	
+	public static final int ADC_READ = 0x00;
+	public static final int ADC_CONVERSION = 0x48;
 	@Override
 	public void init() {
 		// TODO Auto-generated method stub
