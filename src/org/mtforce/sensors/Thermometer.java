a73094f78d21facde425d718b78b11976a064b8a
Philip Lindner <philip260897@gmail.com>
2015-11-09 15:42:51 +0100

    Added Constants and Comments

--- src/org/mtforce/sensors/Thermometer.java
+++ src/org/mtforce/sensors/Thermometer.java
@@ -19,6 +19,19 @@ public class Thermometer extends Sensor
 	public static final int CONF_ALERT_MOD	=	0x0001;	//ALERT MODE 1 INTERRUPT,0 COMPARATOR
 	
 	
+	public static final int REG_RES			=	0x08;	//RESOLUTION REGISTER ADDRESS
+	public static final int RES_5			=	0x00;	//RESOLUTION 0.5°C		30ms
+	public static final int RES_25			=	0x01;	//RESOLUTION 0.25°C		65ms
+	public static final int RES_125			=	0x02;	//RESOLUTION 0.125°C	130ms
+	public static final int RES_0625		=	0x03;	//RESOLUTION 0.0625°C	250ms
+	
+	public static final int REG_TUPPER		=	0x02;	//SET T_UPPER LIMIT		(11-2)bit TEMP Value / 12bit Sign
+	public static final int REG_TLOWER		=	0x03;	//SET T_LOWER LIMIT		(11-2)bit TEMP Value / 12bit Sign
+	public static final int REG_TCRIT		=	0x04;	//SET T_CRITICAL LIMIT	(11-2)bit TEMP Value / 12bit Sign
+	public static final int REG_TA			=	0x05;	//GET TEMPERATURE		(11-0)bit TEMP Value / 15bit Ta vs Tcrit / 14bit Ta vs Tupper / 13bit Ta vs Tlower
+	public static final int REG_MANUF_ID	=	0x06;	//MANUFACTURER ID		(16bit)
+	public static final int REG_DEV_ID		=	0x07;	//DEVICE ID				(15-8)bit DEVICE ID / (7-0)bit DEVICE REVISION
+	
 	@Override
 	public void init() {
 		// TODO Auto-generated method stub
