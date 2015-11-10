c73efa5a5b0397d005ed4755593e339f79a72979
Ben Brix <de.mon@live.at>
2015-11-10 16:23:21 +0100

    added write(byte, byte) added Utils class edited addresses in barometer class

--- src/org/mtforce/sensors/Barometer.java
+++ src/org/mtforce/sensors/Barometer.java
@@ -1,18 +1,21 @@
 package org.mtforce.sensors;
 
+import org.mtforce.interfaces.I2CManager;
+
 public class Barometer extends Sensor {
 
-	public static final int ADDRESS = 0x76; //LAST BIT = !CSB (CSB = HARDWARE PIN)
-	public static final int RESET 	= 0x1E;
+	public static final byte ADDRESS = 0x76;		 	//LAST BIT = !CSB (CSB = HARDWARE PIN)
+	public static final byte RESET 	= 0x1E; 		//RESETS PROM
 	
-	public static final int PROM_READ = 0xA6;
+	public static final int PROM_READ = 0xA6;		//8-BIT DATA FOLLOWED BY ACK 2 TIMES (16BIT)
 	
-	public static final int ADC_READ = 0x00;
-	public static final int ADC_CONVERSION = 0x48;
+	public static final byte ADC_CONVERSION = 0x48; 	//CONVERSION OF ANALOG IN
+	public static final byte ADC_READ = 0x00;		//8-BIT DATA FOLLOWED BY ACK 3 TIMES (24BIT)
+	
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
 		
+		I2CManager.write((byte)ADDRESS, (byte)RESET);
 	}
 
 	@Override
