2151a4a0b0298faaaa9a71f172f1cc3923802227
Ben Brix <de.mon@live.at>
2015-11-16 12:49:28 +0100

    Utils comments

--- src/org/mtforce/main/Utils.java
+++ src/org/mtforce/main/Utils.java
@@ -1,14 +1,14 @@
 package org.mtforce.main;
 
 public class Utils {
-	public static byte[] toBytes(int integer, int var){
+	public static byte[] toBytes(int integer, int var){ // returns bytearray
 		byte lastByte[] = new byte[var];
 		for(int i = 0; i<var; i++){
 			lastByte[i] = (byte) (integer >> 24-8*(i+var)); // shifts right every iteration and copies in byte array
 		}return lastByte;
 	}
 	
-	public static int toInt(byte []b){
+	public static int toInt(byte []b){ //returns integer from Bytearray (max 32bit)
 		int combInt = 0;
 		for (int i=0; i<b.length; i++) {
 			combInt <<= 8;				// shifts Int left
