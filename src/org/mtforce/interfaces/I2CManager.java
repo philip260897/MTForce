f7514ae0d8cf96fa4ddad59bb1c4a6ba622f5bf6
Philip Lindner <philip260897@gmail.com>
2015-11-16 13:08:20 +0100

    isBitSet added

--- src/org/mtforce/interfaces/I2CManager.java
+++ src/org/mtforce/interfaces/I2CManager.java
@@ -6,6 +6,8 @@ import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
 import com.pi4j.wiringpi.I2C;
 import com.pi4j.wiringpi.Spi;
+
+
 public class I2CManager 
 {	
 	private static I2CBus bus;
@@ -44,22 +46,6 @@ public class I2CManager
 		}
 		return true;
 	}	
-	
-	public static boolean write(byte address, byte...val)
-	{
-		try
-		{
-			current = bus.getDevice(address);
-			current.write(val[0]);
-			for(int i = 1;i < val.length;i++)
-				current.write(val[i]);
-		}
-		catch(Exception ex)
-		{
-			return false;
-		}
-		return true;
-	}
 	
 	public static boolean write(byte address, byte reg, byte...val)
 	{
--- src/org/mtforce/main/Utils.java
+++ src/org/mtforce/main/Utils.java
@@ -7,13 +7,19 @@ public class Utils {
 			lastByte[i] = (byte) (integer >> 24-8*(i+var)); // shifts right every iteration and copies in byte array
 		}return lastByte;
 	}
-	
+
 	public static int toInt(byte []b){ //returns integer from Bytearray (max 32bit)
 		int combInt = 0;
 		for (int i=0; i<b.length; i++) {
 			combInt <<= 8;				// shifts Int left
 			combInt |= (int)b[i];		// bitwise-OR with int (can be replaced with "+")
-		  }	
+		}	
 		return combInt;
-		}
+	}
+
+	public static boolean isBitSet(byte b, int i) //Check if bit at given index i is set or not
+	{
+		byte mask = (byte) Math.pow(2, i);
+		return ((b & mask) != 0);
+	}
 }
