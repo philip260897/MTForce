08f79428dbe32816eafc3697d94b4e89fe11970b
Ben Brix <de.mon@live.at>
2015-11-23 21:27:53 +0100

    added constants

--- src/org/mtforce/sensors/Barometer.java
+++ src/org/mtforce/sensors/Barometer.java
@@ -5,13 +5,13 @@ import org.mtforce.main.Utils;
 
 public class Barometer extends Sensor {
 
-	public static final byte ADDRESS 		= 0x76;	//LAST BIT = !CSB (CSB = HARDWARE PIN)
-	public static final byte RESET 			= 0x1E; //RESETS PROM
+	public static final byte ADDRESS 		= 0x76;			//LAST BIT = !CSB (CSB = HARDWARE PIN)
+	public static final byte RESET 			= 0x1E; 		//RESETS PROM
 	
 	public static final byte PROM_READ 		= (byte) 0xA6;	//8-BIT DATA FOLLOWED BY ACK 2 TIMES (16BIT)
 	
-	public static final byte ADC_CONVERSION = 0x48; //CONVERSION OF ANALOG IN
-	public static final byte ADC_READ 		= 0x00;	//8-BIT DATA FOLLOWED BY ACK 3 TIMES (24BIT)
+	public static final byte ADC_CONVERSION = 0x48; 		//CONVERSION OF ANALOG IN
+	public static final byte ADC_READ 		= 0x00;			//8-BIT DATA FOLLOWED BY ACK 3 TIMES (24BIT)
 	
 	@Override
 	public void init() {
