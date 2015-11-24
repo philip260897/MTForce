b5a161a6126538e359487edceb57dd8010a7f89c
Ben Brix <de.mon@live.at>
2015-11-24 16:30:16 +0100

    added constants

--- src/org/mtforce/sensors/EnOceanPi.java
+++ src/org/mtforce/sensors/EnOceanPi.java
@@ -2,7 +2,18 @@ package org.mtforce.sensors;
 
 public class EnOceanPi extends Sensor {
 
-	public static final byte ADDRESS = 0x00;
+	public static final byte RESERVED_1 		= 0x00; //RESERVED
+	public static final byte RADIO 				= 0x01; //Radio telegram
+	public static final byte RESPONSE 			= 0x02; //Response to any packet
+	public static final byte RADIO_SUB_TEL 		= 0x03; //Radio subtelegram
+	public static final byte EVENT 				= 0x04; //Event message
+	public static final byte COMMON_COMMAND 	= 0x05; //Common command
+	public static final byte SMART_ACK_COMMAND 	= 0x06; //Smart Ack command
+	public static final byte REMOTE_MAN_COMMAND = 0x07; //Remote management command
+	public static final byte RESERVED_2 		= 0x08; //Reserved for EnOcean
+	public static final byte RADIO_MESSAGE 		= 0x09; //Radio message
+	public static final byte RADIO_ADVANCED 	= 0x0A; //Advanced protocol radio telegram
+
 	
 	@Override
 	public void init() {
