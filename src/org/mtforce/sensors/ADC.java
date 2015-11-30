a40c7c6aa43142013e98a38de9917103824c4ff7
Ben Brix <de.mon@live.at>
2015-11-30 13:32:07 +0100

    Added Constants and comments

--- src/org/mtforce/sensors/ADC.java
+++ src/org/mtforce/sensors/ADC.java
@@ -1,5 +1,39 @@
 package org.mtforce.sensors;
 
-public class ADC {
+public class ADC extends Sensor {
+	public static final byte PGA_GAIN_x1		= 0x00;	//PGA Gain Selection Bits PGA=1
+	public static final byte PGA_GAIN_x2 		= 0x01;	//PGA Gain Selection Bits, PGA=2
+	public static final byte PGA_GAIN_x4 		= 0x02;	//PGA Gain Selection Bits, PGA=4
+	public static final byte PGA_GAIN_x8 		= 0x03;	//PGA Gain Selection Bits, PGA=8
+	public static final byte 240_SPS			= 0x00;	//Sample Rate Selection Bit, 240 SPS, 12 bits
+	public static final byte 60_SPS 			= 0x04;	//Sample Rate Selection Bit, 60 SPS, 14 bits
+	public static final byte 15_SPS 			= 0x08;	//Sample Rate Selection Bit, 15 SPS, 16 bits
+	public static final byte ONE_SHOT_CONV 		= 0x00;	//Conversion Mode Bit, One-Shot Conversion Mode
+	public static final byte CONTINUOUS_CONV 	= 0x10;	//Conversion Mode Bit, Continuous Conversion Mode
+	public static final byte SELECT_CH1 		= 0x00;	//Channel Selection Bits, Select Channel 1
+	public static final byte SELECT_CH2 		= 0x20;	//Channel Selection Bits, Select Channel 2
+	public static final byte SELECT_CH3 		= 0x40;	//Channel Selection Bits, Select Channel 3
+	public static final byte SELECT_CH4 		= 0x60;	//Channel Selection Bits, Select Channel 4
+	public static final byte NO_EFFECT 			= 0x00;	//One-Shot Conversion mode, No effect
+	public static final byte INIT_NEW_CONV 		= 0x80;	//One-Shot Conversion mode, Initiate a new conversion.
 
+	
+	
+	
+	
+	@Override
+	public void init() {
+		
+	}
+
+	@Override
+	public void update() {
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/org/mtforce/sensors/HumiditySensor.java
+++ src/org/mtforce/sensors/HumiditySensor.java
@@ -1,7 +1,7 @@
 package org.mtforce.sensors;
 
 public class HumiditySensor extends Sensor {
-	public final static byte ADDRESS 				= (byte) 0x80; 	//DEVICE ADDRESS
+	public final static byte ADDRESS 				= (byte) 0x80; 		//DEVICE ADDRESS
 	public final static byte MEASURE_HUM_HMM 		= (byte) 0xE5;		//Measure Relative Humidity, Hold Master Mode
 	public final static byte MEASURE_HUM_NO_HMM 	= (byte) 0xF5;		//Measure Relative Humidity, No Hold Master Mode
 	public final static byte MT_HMM 				= (byte) 0xE3;		//Measure Temperature, Hold Master Mode
@@ -10,12 +10,12 @@ public class HumiditySensor extends Sensor {
 	public final static byte RESET 					= (byte) 0xFE;		//Reset
 	public final static byte WRITE_RHT_REG1 		= (byte) 0xE6;		//Write RH/T User Register 1
 	public final static byte READ_RHT_REG1 			= (byte) 0xE7;		//Read RH/T User Register 1
-	public final static byte WRITE_HEATER_CTRL_REG 	= 0x51;		//Write Heater Control Register
-	public final static byte READ_HEATER_CTRL_REG 	= 0x11;		//Read Heater Control Register
-	public final static int READ_ID_1ST_BYTE 		= 0xFA0F;	//Read Electronic ID 1st Byte
-	public final static int WRITE_ID_1ST_BYTE 		= 0xFCC9;	//Read Electronic ID 2nd Byte
-	public final static int READ_FIRMWARE_REV		= 0x84B8;	//Read Firmware Revision
-
+	public final static byte WRITE_HEATER_CTRL_REG 	= 0x51;				//Write Heater Control Register
+	public final static byte READ_HEATER_CTRL_REG 	= 0x11;				//Read Heater Control Register
+	public final static int  READ_ID_1ST_BYTE 		= 0xFA0F;			//Read Electronic ID 1st Byte
+	public final static int  WRITE_ID_1ST_BYTE 		= 0xFCC9;			//Read Electronic ID 2nd Byte
+	public final static int  READ_FIRMWARE_REV		= 0x84B8;			//Read Firmware Revision
+	
 	
 	@Override
 	public void init() {
